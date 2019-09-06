db7d8621a38f3459938f4e45da842ec62d5b41b9
fwcd <fwcdmail@gmail.com>
2019-09-06 16:57:46 +0200

    Create skeletal Electron application

---
+++ src/main.js
@@ -0,0 +1,27 @@
+const {app, BrowserWindow} = require("electron");
+const path = require("path");
+
+let mainWindow;
+
+function createWindow() {
+	mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+		autoHideMenuBar: true
+	});
+	mainWindow.loadFile(path.join(__dirname, "index.html"));
+	mainWindow.on("closed", () => mainWindow = null);
+}
+
+app.on("ready", createWindow);
+app.on("window-all-closed", () => {
+	// On macOS applications conventionally stay active in the background.
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
+});
+app.on("activate", () => {
+	if (mainWindow != null) {
+		createWindow();
+	}
+});
