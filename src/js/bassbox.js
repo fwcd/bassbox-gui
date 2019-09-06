19ee1b14ada21604ad57f81000d169de4a86fed6
fwcd <fwcdmail@gmail.com>
2019-09-06 20:01:04 +0200

    Use ES6 proxies to model the remote JSON-RPC endpoint

--- src/js/bassbox.js
+++ src/js/bassbox.js
@@ -6,17 +6,8 @@ const { jsonRpcProxyFrom } = require("./jsonrpc");
 function launchBassbox() {
 	const exeName = `bassbox${process.platform === "win32" ? ".exe" : ""}`
 	const proc = childProcess.execFile(path.join(__dirname, "..", "..", "local", exeName), ["-e", "speaker"]);
-	const proxy = jsonRpcProxyFrom(proc.stdout, proc.stdin);
-	return {
-		audioPlayer: {
-			enqueueFile(path) { return proxy.call("audioPlayer.enqueueFile", [path]); },
-			play() { return proxy.call("audioPlayer.play", []); },
-			pause() { return proxy.call("audioPlayer.pause", []); },
-			setVolume(volume) { return proxy.call("audioPlayer.setVolume", [volume]); },
-			setLowpassEnabled(enabled) { return proxy.call("audioPlayer.setLowpassEnabled", [enabled]); },
-			setHighpassEnabled(enabled) { return proxy.call("audioPlayer.setHighpassEnabled", [enabled]); }
-		}
-	};
+	const services = ["audioPlayer"];
+	return jsonRpcProxyFrom(proc.stdout, proc.stdin, services);
 }
 
 module.exports = { launchBassbox };
--- src/js/jsonrpc.js
+++ src/js/jsonrpc.js
@@ -1,34 +1,52 @@
 const { lineReader } = require("./utils");
 
-function jsonRpcProxyFrom(procOut, procIn) {
-	const resolvers = {};
-	let id = 0;
+/**
+ * A proxy object on which RPC methods can be invoked
+ * as if they were present on a normal object.
+ */
+function scopedProxy(context, scope) {
+	return new Proxy({}, {
+		get(target, name) {
+			const fqName = (scope.length > 0) ? `${scope}.${name}` : name;
+			if (context.scopes.includes(name)) {
+				return scopedProxy(context, fqName);
+			} else {
+				return (...args) => new Promise(resolve => {
+					let requestId = context.id++;
+					context.resolvers[requestId] = resolve;
+					context.procIn.write(JSON.stringify({
+						jsonrpc: "2.0",
+						id: requestId,
+						method: fqName,
+						params: args
+					}) + "\n");
+				});
+			}
+		}
+	});
+}
+
+/** Launches a new JSON-RPC client and returns the server proxy. */
+function jsonRpcProxyFrom(procOut, procIn, scopes) {
+	const context = {
+		scopes,
+		procIn,
+		resolvers: {},
+		id: 0
+	};
 	
 	lineReader(procOut).onLine(line => {
 		const raw = line.trim();
 		const response = JSON.parse(raw);
-		if (response.id in resolvers) {
-			resolvers[response.id](response);
-			delete resolvers[response.id];
+		if (response.id in context.resolvers) {
+			context.resolvers[response.id](response);
+			delete context.resolvers[response.id];
 		} else {
 			console.log(`Warning: Got unrecognized response ${raw}`);
 		}
 	});
-
-	return {
-		call(method, args) {
-			return new Promise(resolve => {
-				let requestId = id++;
-				resolvers[requestId] = resolve;
-				procIn.write(JSON.stringify({
-					jsonrpc: "2.0",
-					id: requestId,
-					method: method,
-					params: args
-				}) + "\n");
-			});
-		}
-	};
+	
+	return scopedProxy(context, "");
 }
 
 module.exports = { jsonRpcProxyFrom };
