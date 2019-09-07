9c989e253e438fac662373a74f26f5fab92b0122
fwcd <fwcdmail@gmail.com>
2019-09-08 00:17:37 +0200

    Use Klay layouting

--- src/js/renderer.js
+++ src/js/renderer.js
@@ -1,7 +1,10 @@
 const cytoscape = require("cytoscape");
+const klay = require("cytoscape-klay");
 const fs = require("fs");
 const path = require("path");
 const { launchBassbox } = require("./bassbox");
+
+cytoscape.use(klay);
 
 async function loadStylesheet() {
 	return await fs.promises.readFile(path.join(__dirname, "..", "css", "graph.css"), { encoding: "utf8" });
@@ -13,7 +16,14 @@ async function setup() {
 	const cy = cytoscape({
 		container: document.getElementById("graphview"),
 		elements: graph.nodes.map((node, i) => ({ data: { id: `${i}`, label: node.type } }))
-			.concat(graph.edges.map(edge => ({ data: { id: `${edge.src}->${edge.dest}`, source: `${edge.src}`, target: `${edge.dest}` } })))
+			.concat(graph.edges.map(edge => ({ data: { id: `${edge.src}->${edge.dest}`, source: `${edge.src}`, target: `${edge.dest}` } }))),
+		layout: {
+			name: "klay",
+			animate: true,
+			klay: {
+				spacing: 120
+			}
+		}
 	});
 	cy.style(await loadStylesheet());
 }
