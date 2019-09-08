1d885d5e68d357038d10a34204634e9f4d722adb
fwcd <fwcdmail@gmail.com>
2019-09-09 00:00:56 +0200

    Add Popper.js

--- src/js/graphView.js
+++ src/js/graphView.js
@@ -1,17 +1,19 @@
 const cytoscape = require("cytoscape");
 const klay = require("cytoscape-klay");
+const popper = require("cytoscape-popper");
 const fs = require("fs");
 const path = require("path");
 const { launchBassbox } = require("./bassbox");
 
 cytoscape.use(klay);
+cytoscape.use(popper);
 
 async function loadStylesheet() {
 	return await fs.promises.readFile(path.join(__dirname, "..", "css", "graph.css"), { encoding: "utf8" });
 }
 
 function toCytoNode(node, i) {
-	return { data: { id: `${i}`, label: node.type } };
+	return { data: { id: `${i}`, label: node.type, node: node } };
 }
 
 function toCytoEdge(edge) {
@@ -34,7 +36,7 @@ async function createGraphView(element) {
 		}
 	});
 	cy.style(await loadStylesheet());
-
+	
 	const handler = {
 		async addNode(node, cytoOpts) {
 			const index = await bassbox.audioGraph.addNode(node);
@@ -46,7 +48,18 @@ async function createGraphView(element) {
 		}
 	};
 	cy.on("click", async e => {
-		await handler.addNode({ type: "Empty" }, { position: e.position });
+		if (e.target == null) {
+			await handler.addNode({ type: "Empty" }, { position: e.position });
+		} else if (("group" in e.target) && e.target.group() == "nodes") {
+			e.target.popper({
+				content: () => {
+					const div = document.createElement("div");
+					div.innerText = JSON.stringify(e.target.data().node);
+					document.body.appendChild(div);
+					return div;
+				}
+			});
+		}
 	});
 }
 
